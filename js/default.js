644b881872af10ed2037a6887977d9bf0fdd8d81
miyuhann <tennkunn96@gmail.com>
2017-07-03 17:23:50 +0900

    Create default.js

---
+++ js/default.js
@@ -0,0 +1,18 @@
+var separate_time = function(time){
+var sec = Math.floor((time / 1000) % 60);
+var min = Math.floor((time / 1000 / 60) % 60);
+var hours = Math.floor((time / 1000 / 60 / 60) % 24);
+var days = Math.floor(time / 1000 / 60 / 60 / 24);
+return [sec,min,hours,days];
+}
+
+var now = new Date();
+var target = new Date(2020,7,24,0,0,0,0);
+var diff = target.getTime() - now.getTime();
+var couter = separate_time(diff);
+docment.getElwmentById('countdown').textContent =
+'東京オリンピックまであと'　+
+counter[3] + '日' +
+counter[2] + '時' +
+counter[1] + '分' +
+counter[0] + '秒'；
