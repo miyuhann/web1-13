efa387deb261150b10d1ad8565240cea2883379b
miyuhann <tennkunn96@gmail.com>
2017-07-03 17:27:10 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -6,6 +6,7 @@ var days = Math.floor(time / 1000 / 60 / 60 / 24);
 return [sec,min,hours,days];
 }
 
+var update = function(){
 var now = new Date();
 var target = new Date(2020,7,24,0,0,0,0);
 var diff = target.getTime() - now.getTime();
@@ -16,3 +17,10 @@ counter[3] + '日' +
 counter[2] + '時' +
 counter[1] + '分' +
 counter[0] + '秒'；
+refresh();
+}
+
+var refresh= function(){
+  setTimeout(update,1000);
+}
+update();
